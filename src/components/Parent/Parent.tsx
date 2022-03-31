02b2050e76daa3c84f01ed307edddba81792f808
MalcolmDahling <mac.dahling@gmail.com>
2022-03-31 23:49:26 +0200

    Update Parent.tsx

--- src/components/Parent/Parent.tsx
+++ src/components/Parent/Parent.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Container = styled.div`
     border: 1px solid grey;
@@ -17,17 +17,38 @@ const Button = styled.input.attrs({ type: 'button', value: 'Button' })`
 
 
 
-const Div = styled.div.attrs(props => ({
-    size: props.size || '1em'
-}))`
+// const Div = styled.div.attrs(props => ({
+//     size: props.size || '1em'
+// }))`
 
-    background-color: red;
-    width: 200px;
-    height: 200px;
+//     background-color: red;
+//     width: 200px;
+//     height: 200px;
 
-    margin: ${props => props.size};
-    padding: ${props => props.size};
+//     margin: ${props => props.size};
+//     padding: ${props => props.size};
+// `;
+
+
+const stretchAnim = keyframes`
+    0% { width:200px; }
+    50% { width:400px; }
+    100% { width:200px; }
 `;
+
+const AnimatedDiv = styled.div`
+    width:200px;
+    height:200px;
+    background-color:blue;
+    margin:auto;
+    margin-top:10px;
+
+    animation-name: ${stretchAnim};
+    animation-duration: 2s;
+    animation-iteration-count: infinite;
+`;
+
+
 
 
 export function Parent(){
@@ -39,7 +60,10 @@ export function Parent(){
             <Heading>Rubrik</Heading>
             <Button color={buttonColor}></Button>
 
-            <Div size="20px">asdf</Div>
+            {/* <Div size="20px">asdf</Div> */}
+
+            <AnimatedDiv></AnimatedDiv>
+
         </Container>
     );
 }
